7baf7bb52483645e5d3568395c4683cc8ae53082
Kirill <kerlykiv@gmail.com>
2023-03-12 18:59:30 +0700

    long poiling cats

--- file.py
+++ file.py
@@ -8,13 +8,18 @@ BOT_TOKEN: str = '6002753499:AAHPsD8ZL_0dyvNfpMWgPxhJC22NZImPOTk'
 TEXT_ERROR: str = 'Котики заняты'
 
 offset: int = -2
-counter: int = 0
+timeout: int = 60
+updates: dict
 cat_response: requests.Response
 chat_id: int
 
 
-while counter < 100:
-    print('attempt =', counter)
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True:
+    start_time = time.time()
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
@@ -27,6 +32,7 @@ while counter < 100:
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
             else:
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={TEXT_ERROR}')
+            do_something()
 
-    time.sleep(1)
-    counter += 1
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
