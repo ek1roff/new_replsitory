c388b8f722db9d3772eaf3b7e36b895dd0990c57
Kirill <kerlykiv@gmail.com>
2023-03-13 16:26:16 +0700

    Echo-bot

--- file.py
+++ file.py
@@ -1,38 +1,31 @@
-import requests
-import time
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+API_TOKEN: str = '6002753499:AAHPsD8ZL_0dyvNfpMWgPxhJC22NZImPOTk'
+
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
 
 
-API_URL: str = 'https://api.telegram.org/bot'
-API_CAT_URL: str = 'https://aws.random.cat/meow'
-BOT_TOKEN: str = '6002753499:AAHPsD8ZL_0dyvNfpMWgPxhJC22NZImPOTk'
-TEXT_ERROR: str = 'Котики заняты'
-
-offset: int = -2
-timeout: int = 60
-updates: dict
-cat_response: requests.Response
-chat_id: int
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message):
+    await message.answer('Провет!\nЯ Эхо-бот.\nНапиши мне что-нибудь!')
 
 
-def do_something() -> None:
-    print('Был апдейт')
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь,\nа я повторю.')
 
 
-while True:
-    start_time = time.time()
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Данный тип апдейтов не поддерживается '
+                                 'методом send_copy')
 
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CAT_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()['file']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={TEXT_ERROR}')
-            do_something()
 
-    end_time = time.time()
-    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
+if __name__ == '__main__':
+    dp.run_polling(bot)
