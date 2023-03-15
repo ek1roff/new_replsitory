db074a9b0fa58aa89ffabe83ae35f38b05bb0207
Kirill <kerlykiv@gmail.com>
2023-03-15 12:54:13 +0700

    111

--- file.py
+++ file.py
@@ -11,17 +11,20 @@ dp: Dispatcher = Dispatcher()
 @dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
     await message.answer('Провет!\nЯ Эхо-бот.\nНапиши мне что-нибудь!')
+    print(message.json(exclude_none=True, indent=4))
 
 
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь,\nа я повторю.')
+    print(message.json(exclude_none=True, indent=4))
 
 
 @dp.message()
 async def send_echo(message: Message):
     try:
         await message.send_copy(chat_id=message.chat.id)
+        print(message.json(exclude_none=True, indent=4))
     except TypeError:
         await message.reply(text='Данный тип апдейтов не поддерживается '
                                  'методом send_copy')
